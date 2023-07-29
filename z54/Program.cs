c2d6bba9b197a7fc24536e02ea8be5c6754f8c18
Anton <antonrybakov95@yandex.ru@yandex.ru>
2023-07-29 17:00:45 +0800

    Задача 54

---
+++ z54/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+Console.WriteLine("Ведите кол-во строчек: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввдетие кол-во колонок: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+
+void GetArray()
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i,j]+ " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void ChangeArray()
+{
+    int temp = 0;
+    int SortRows = matrix.GetLength(1) - 1;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+GetArray();
+PrintArray();
+ChangeArray();
+System.Console.WriteLine("__________");
+PrintArray();
